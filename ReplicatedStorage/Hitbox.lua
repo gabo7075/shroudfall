36a7034f425940e0a57ef26aab80bf84423a50b6
Gabri7075 <gabridelsol@gmail.com>
2026-01-16 00:50:10 -0400

    hitbox shape

--- ReplicatedStorage/Hitbox.lua
+++ ReplicatedStorage/Hitbox.lua
@@ -5,7 +5,7 @@ local replicatedStorage = game:GetService("ReplicatedStorage")
 
 local module = {}
 
-function module.create(cframe: CFrame, size: Vector3, duration: number)
+function module.create(cframe: CFrame, size: Vector3, duration: number, shape: Enum.PartType?)
 	local hitbox = Instance.new("Part")
 	hitbox.CanCollide = false
 	hitbox.CastShadow = false
@@ -13,21 +13,28 @@ function module.create(cframe: CFrame, size: Vector3, duration: number)
 	hitbox.CFrame = cframe
 	hitbox.Size = size
 	hitbox.BrickColor = BrickColor.new("Really red")
+
+	-- Set shape, default to Block if nil
+	hitbox.Shape = shape or Enum.PartType.Block
+
 	if replicatedStorage.Configuration.VisibleHitboxxes.Value == false then
 		hitbox.Transparency = 1
 	end
 	hitbox.Parent = workspace.GameDebris
+
 	runService.RenderStepped:Connect(function()
 		if hitbox then
 			hitbox.CFrame = cframe
 			hitbox.Velocity = Vector3.zero
 		end
 	end)
+
 	task.delay(duration, function()
 		hitbox.CanTouch = false
 		tweenService:Create(hitbox, TweenInfo.new(1, Enum.EasingStyle.Linear), {Transparency = 1}):Play()
 		debris:AddItem(hitbox, 1)
 	end)
+
 	return hitbox
 end
 
